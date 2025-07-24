b1a949ac65ce03afcfb43a90824ae869952ebc14
yavuzbeydogan <yavuz.beydogan@hotmail.com>
2025-07-24 10:39:14 +0300

    login register backend düzeltildi.

--- AuthECAPI/Program.cs
+++ AuthECAPI/Program.cs
@@ -10,6 +10,7 @@ using System.Security.Claims;
 using System.Text; // <-- Eksik
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:5084");
 
 // Add services to the container.
 
